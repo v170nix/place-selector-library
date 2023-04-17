e4339394546ff40139d8d1feb0378b6a403ba0d7
Vitaliy Sheyanov <vit.onix@gmail.com>
2023-04-18 00:40:53 +0300

    init commit

---
+++ demo/src/main/java/net/arwix/placeselectorfeature/parts/edit/timezone/ui/PlaceEditTimeZoneScreen.kt
@@ -0,0 +1,9 @@
+package net.arwix.placeselectorfeature.parts.edit.timezone.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PlaceEditTimeZoneScreen() {
+    Text("time zone")
+}
